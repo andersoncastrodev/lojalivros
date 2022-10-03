5bf3228a72b15d76c4fec02e8bdb20cc195ab844
Anderson Castro <andersoncastrosantos@gmail.com>
2022-10-03 20:02:31 -0300

    Adicionando banco de dados.

--- src/main/java/br/com/LojalivrosApplication.java
+++ src/main/java/br/com/LojalivrosApplication.java
@@ -1,13 +1,45 @@
 package br.com;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojalivrosApplication {
+import br.com.domian.Categoria;
+import br.com.domian.Livro;
+import br.com.repository.CategoriaRepository;
+import br.com.repository.LivroRepository;
 
+@SpringBootApplication
+public class LojalivrosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojalivrosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria = new Categoria(null, "Informatica", "Livros de TI");
+		
+		Livro livro = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", categoria);
+		
+		categoria.getLivros().addAll( Arrays.asList(livro) );
+		
+		categoriaRepository.saveAll( Arrays.asList(categoria) );
+		
+		livroRepository.saveAll( Arrays.asList(livro) );
+		
 	}
 
 }
--- src/main/java/br/com/repository/LivroRepository.java
+++ src/main/java/br/com/repository/LivroRepository.java
@@ -1,8 +1,11 @@
 package br.com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import br.com.domian.Livro;
 
+@Repository
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
 	
 }
