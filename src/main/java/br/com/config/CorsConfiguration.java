798e8b4f53415af514d7e994794c8fd8f11e94c2
Anderson Castro <andersoncastrosantos@gmail.com>
2022-10-09 19:08:16 -0300

    alterando a classe Cors

--- src/main/java/br/com/config/CorsConfiguration.java
+++ src/main/java/br/com/config/CorsConfiguration.java
@@ -1,19 +1,23 @@
 package br.com.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration {
 
 	//Permitindo o Front-End LocalHost com Angular 10
-	public void addCorsMapping(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:4200/")
-		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+			}
+		};
 		
 	}
 	
