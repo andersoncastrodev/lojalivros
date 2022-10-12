1fa7ec5e041d1db6e5e2f516f19e9d78955c8dd4
Anderson Castro <andersoncastrosantos@gmail.com>
2022-10-12 15:34:25 -0300

    adicionando parametros a classe CorsConfiguration

--- src/main/java/br/com/config/CorsConfiguration.java
+++ src/main/java/br/com/config/CorsConfiguration.java
@@ -15,7 +15,8 @@ public class CorsConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200/")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT","COPY");
 			}
 		};
 		
